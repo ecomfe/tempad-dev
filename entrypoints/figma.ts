2e2d7e94ae935bf34b93baef88aeee33480a394a
Justineo <justice360@gmail.com>
2024-11-07 22:47:31 +0800

    refactor: format figma.ts

--- entrypoints/figma.ts
+++ entrypoints/figma.ts
@@ -1,36 +1,35 @@
 export default defineUnlistedScript(async () => {
-	const markers = [
-		'delete window.figma',
-		'.createAPI()'
-	]
+  const markers = ['delete window.figma', '.createAPI()']
 
-	const current = document.currentScript as HTMLScriptElement
-	const src = current.src
+  const current = document.currentScript as HTMLScriptElement
+  const src = current.src
 
-	function replaceScript(src: string) {
-		const script = document.createElement('script');
-		script.src = src;
-		script.defer = true;
-		current.replaceWith(script);
-	}
+  function replaceScript(src: string) {
+    const script = document.createElement('script')
+    script.src = src
+    script.defer = true
+    current.replaceWith(script)
+  }
 
-	function matchFile(content: string) {
-		return markers.every((marker) => content.includes(marker))
-	}
+  function matchFile(content: string) {
+    return markers.every((marker) => content.includes(marker))
+  }
 
-	try {
-		let content = await (await fetch(src)).text()
+  try {
+    let content = await (await fetch(src)).text()
 
-		if (matchFile(content)) {
-			content = content.replace(/if\(!([a-zA-Z\d]+)\.userID\|\|/, 'if(true){}else if(!$1.userID||')
-			content = content.replaceAll('delete window.figma', 'window.figma = undefined')
-		}
+    if (matchFile(content)) {
+      content = content.replace(/if\(!([a-zA-Z\d]+)\.userID\|\|/, 'if(true){}else if(!$1.userID||')
+    }
 
-		// document.currentScript will be `null` if we run with `new Function()`
-		content = content.replaceAll('document.currentScript.src', `"${src}"`)
+    // document.currentScript will be `null` if we run with `new Function()`
+    content = content.replaceAll('document.currentScript.src', `"${src}"`)
 
-		new Function(content)()
-	} catch (_) {
-		replaceScript(`${src}?fallback`)
-	}
+    // delete window.figma may throw Error in strict mode
+    content = content.replaceAll('delete window.figma', 'window.figma = undefined')
+
+    new Function(content)()
+  } catch (_) {
+    replaceScript(`${src}?fallback`)
+  }
 })
