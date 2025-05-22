a2fe5f540d9dc753bf52a132665687a627bc69a7
Justineo <justice360@gmail.com>
2025-05-22 13:51:01 +0800

    fix: fix rewrite script and redirect url

--- rewrite/figma.ts
+++ rewrite/figma.ts
@@ -1,29 +1,33 @@
 import { matchFile, REWRITE_PATTERN, REWRITE_REPLACER } from './config'
 
-const current = document.currentScript as HTMLScriptElement
-const src = current.src
+async function rewriteScript() {
+  const current = document.currentScript as HTMLScriptElement
+  const src = current.src
 
-function replaceScript(src: string) {
-  const script = document.createElement('script')
-  script.src = src
-  script.defer = true
-  current.replaceWith(script)
-}
-
-try {
-  let content = await (await fetch(src)).text()
-
-  if (matchFile(src, content)) {
-    content = content.replace(REWRITE_PATTERN, REWRITE_REPLACER)
+  function replaceScript(src: string) {
+    const script = document.createElement('script')
+    script.src = src
+    script.defer = true
+    current.replaceWith(script)
   }
 
-  // document.currentScript will be `null` if we run with `new Function()`
-  content = content.replaceAll('document.currentScript.src', `"${src}"`)
+  try {
+    let content = await (await fetch(src)).text()
 
-  // delete window.figma may throw Error in strict mode
-  content = content.replaceAll('delete window.figma', 'window.figma = undefined')
+    if (matchFile(src, content)) {
+      content = content.replace(REWRITE_PATTERN, REWRITE_REPLACER)
+    }
 
-  new Function(content)()
-} catch {
-  replaceScript(`${src}?fallback`)
+    // document.currentScript will be `null` if we run with `new Function()`
+    content = content.replaceAll('document.currentScript.src', `"${src}"`)
+
+    // delete window.figma may throw Error in strict mode
+    content = content.replaceAll('delete window.figma', 'window.figma = undefined')
+
+    new Function(content)()
+  } catch {
+    replaceScript(`${src}?fallback`)
+  }
 }
+
+rewriteScript()
