5a44db8d96d4e61c0eb8b44a63d20f903f0c6858
Justineo <justice360@gmail.com>
2025-05-23 23:07:20 +0800

    fix: improved rewrite

--- rewrite/figma.ts
+++ rewrite/figma.ts
@@ -4,6 +4,8 @@ async function rewriteScript() {
   const current = document.currentScript as HTMLScriptElement
   const src = current.src
 
+  const desc = Object.getOwnPropertyDescriptor(Document.prototype, 'currentScript')
+
   function replaceScript(src: string) {
     const script = document.createElement('script')
     script.src = src
@@ -16,18 +18,25 @@ async function rewriteScript() {
 
     if (matchFile(src, content)) {
       content = content.replace(REWRITE_PATTERN, REWRITE_REPLACER)
+      console.log(`Rewrote script: ${src}`)
     }
-
-    // document.currentScript will be `null` if we run with `new Function()`
-    content = content.replaceAll('document.currentScript.src', `"${src}"`)
 
     // delete window.figma may throw Error in strict mode
     content = content.replaceAll('delete window.figma', 'window.figma = undefined')
+
+    Object.defineProperty(document, 'currentScript', {
+      configurable: true,
+      get() {
+        return current
+      }
+    })
 
     new Function(content)()
   } catch (e) {
     console.error(e)
     replaceScript(`${src}?fallback`)
+  } finally {
+    Object.defineProperty(document, 'currentScript', desc as PropertyDescriptor)
   }
 }
 
--- wxt.config.ts
+++ wxt.config.ts
@@ -20,10 +20,6 @@ export default defineConfig({
         matches: ['https://www.figma.com/*']
       },
       {
-        resources: ['/figma.js'],
-        matches: ['https://www.figma.com/*']
-      },
-      {
         resources: ['/codegen.js'],
         matches: ['https://www.figma.com/*']
       }
