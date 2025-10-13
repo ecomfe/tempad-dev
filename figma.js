68f031c1ff3422499e4e8edcf4b6368a0c3b77ce
Justineo <Justineo@users.noreply.github.com>
2025-10-13 15:55:08 +0000

    deploy: f1178deda8214bd1893f9ce599ad9f581cd6742d

--- figma.js
+++ figma.js
@@ -1,5 +1,139 @@
 "use strict";
 (() => {
+  // node_modules/.pnpm/p-timeout@6.1.4/node_modules/p-timeout/index.js
+  var TimeoutError = class extends Error {
+    constructor(message) {
+      super(message);
+      this.name = "TimeoutError";
+    }
+  };
+  var AbortError = class extends Error {
+    constructor(message) {
+      super();
+      this.name = "AbortError";
+      this.message = message;
+    }
+  };
+  var getDOMException = (errorMessage) => globalThis.DOMException === void 0 ? new AbortError(errorMessage) : new DOMException(errorMessage);
+  var getAbortedReason = (signal) => {
+    const reason = signal.reason === void 0 ? getDOMException("This operation was aborted.") : signal.reason;
+    return reason instanceof Error ? reason : getDOMException(reason);
+  };
+  function pTimeout(promise, options) {
+    const {
+      milliseconds,
+      fallback,
+      message,
+      customTimers = { setTimeout, clearTimeout }
+    } = options;
+    let timer;
+    let abortHandler;
+    const wrappedPromise = new Promise((resolve, reject) => {
+      if (typeof milliseconds !== "number" || Math.sign(milliseconds) !== 1) {
+        throw new TypeError(`Expected \`milliseconds\` to be a positive number, got \`${milliseconds}\``);
+      }
+      if (options.signal) {
+        const { signal } = options;
+        if (signal.aborted) {
+          reject(getAbortedReason(signal));
+        }
+        abortHandler = () => {
+          reject(getAbortedReason(signal));
+        };
+        signal.addEventListener("abort", abortHandler, { once: true });
+      }
+      if (milliseconds === Number.POSITIVE_INFINITY) {
+        promise.then(resolve, reject);
+        return;
+      }
+      const timeoutError = new TimeoutError();
+      timer = customTimers.setTimeout.call(void 0, () => {
+        if (fallback) {
+          try {
+            resolve(fallback());
+          } catch (error) {
+            reject(error);
+          }
+          return;
+        }
+        if (typeof promise.cancel === "function") {
+          promise.cancel();
+        }
+        if (message === false) {
+          resolve();
+        } else if (message instanceof Error) {
+          reject(message);
+        } else {
+          timeoutError.message = message ?? `Promise timed out after ${milliseconds} milliseconds`;
+          reject(timeoutError);
+        }
+      }, milliseconds);
+      (async () => {
+        try {
+          resolve(await promise);
+        } catch (error) {
+          reject(error);
+        }
+      })();
+    });
+    const cancelablePromise = wrappedPromise.finally(() => {
+      cancelablePromise.clear();
+      if (abortHandler && options.signal) {
+        options.signal.removeEventListener("abort", abortHandler);
+      }
+    });
+    cancelablePromise.clear = () => {
+      customTimers.clearTimeout.call(void 0, timer);
+      timer = void 0;
+    };
+    return cancelablePromise;
+  }
+
+  // node_modules/.pnpm/p-wait-for@5.0.2/node_modules/p-wait-for/index.js
+  var resolveValue = Symbol("resolveValue");
+  async function pWaitFor(condition, options = {}) {
+    const {
+      interval = 20,
+      timeout = Number.POSITIVE_INFINITY,
+      before = true
+    } = options;
+    let retryTimeout;
+    let abort = false;
+    const promise = new Promise((resolve, reject) => {
+      const check = async () => {
+        try {
+          const value = await condition();
+          if (typeof value === "object" && value[resolveValue]) {
+            resolve(value[resolveValue]);
+          } else if (typeof value !== "boolean") {
+            throw new TypeError("Expected condition to return a boolean");
+          } else if (value === true) {
+            resolve();
+          } else if (!abort) {
+            retryTimeout = setTimeout(check, interval);
+          }
+        } catch (error) {
+          reject(error);
+        }
+      };
+      if (before) {
+        check();
+      } else {
+        retryTimeout = setTimeout(check, interval);
+      }
+    });
+    if (timeout === Number.POSITIVE_INFINITY) {
+      return promise;
+    }
+    try {
+      return await pTimeout(promise, typeof timeout === "number" ? { milliseconds: timeout } : timeout);
+    } finally {
+      abort = true;
+      clearTimeout(retryTimeout);
+    }
+  }
+  pWaitFor.resolveWith = (value) => ({ [resolveValue]: value });
+
   // rewrite/config.ts
   var GROUPS = [
     {
@@ -45,6 +179,13 @@
   }
 
   // rewrite/figma.ts
+  async function waitRuntimeReady() {
+    const getQueue = () => window.webpackChunk_figma_web_bundler ||= [];
+    await pWaitFor(() => {
+      const queue = getQueue();
+      return typeof queue.push === "function" && queue.push !== Array.prototype.push;
+    });
+  }
   async function rewriteScript() {
     const current = document.currentScript;
     const src = current?.src;
@@ -66,6 +207,7 @@
         console.log(`Rewrote script: ${src}`);
       }
       const content = afterRules.replaceAll("delete window.figma", "window.figma = undefined");
+      await waitRuntimeReady();
       Object.defineProperty(document, "currentScript", {
         configurable: true,
         get() {
@@ -77,7 +219,11 @@
       console.error(e);
       replaceScript(`${src}?fallback`);
     } finally {
-      Object.defineProperty(document, "currentScript", desc);
+      if (desc) {
+        Object.defineProperty(document, "currentScript", desc);
+      } else {
+        delete document.currentScript;
+      }
     }
   }
   rewriteScript();
