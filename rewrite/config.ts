e0327769c80ddddfb7b00776678dcd1dc7410a5b
Justineo <justice360@gmail.com>
2025-11-19 15:04:27 +0800

    fix: update rewrite patterns

--- rewrite/config.ts
+++ rewrite/config.ts
@@ -11,15 +11,17 @@ export const GROUPS: Group[] = [
     ]
   },
   {
-    markers: ['i=(0,_.rd)()||n;if(!i)'],
+    markers: ['const __html__ = (() => {'],
     replacements: [
       {
-        pattern: 'i=(0,_.rd)()||n;if(!i)',
-        replacer: 'i=(0,_.rd)()||n;if(false)'
+        pattern:
+          /([A-Za-z_$][A-Za-z0-9_$]*)=\(0,([A-Za-z_$][A-Za-z0-9_$]*\.[A-Za-z_$][A-Za-z0-9_$]*)\)\(\)\|\|([A-Za-z_$][A-Za-z0-9_$]*);if\(!\1\)/,
+        replacer: '$1=(0,$2)()||$3;if(false)'
       },
       {
-        pattern: 'if((0,ee.et)())return;i&&',
-        replacer: 'if((0,ee.et)())return;true&&'
+        pattern:
+          /if\(\(0,([A-Za-z_$][A-Za-z0-9_$]*\.[A-Za-z_$][A-Za-z0-9_$]*)\)\(\)\)return;([A-Za-z_$][A-Za-z0-9_$]*)&&/,
+        replacer: 'if((0,$1)())return;true&&'
       }
     ]
   },
