dab64aa2a65f0c7969b0f4a1dd2927664ebf002d
Justineo <Justineo@users.noreply.github.com>
2025-11-20 04:51:28 +0000

    deploy: 1754837ddc69263bffa4f3c12453dc1a8ab9df71

--- figma.js
+++ figma.js
@@ -6,7 +6,7 @@
       markers: [".appModel.isReadOnly"],
       replacements: [
         {
-          pattern: /\.appModel\.isReadOnly/g,
+          pattern: ".appModel.isReadOnly",
           replacer: ".appModel.__isReadOnly__"
         }
       ]
@@ -16,11 +16,11 @@
       replacements: [
         {
           pattern: /([A-Za-z_$][A-Za-z0-9_$]*)=\(0,([A-Za-z_$][A-Za-z0-9_$]*\.[A-Za-z_$][A-Za-z0-9_$]*)\)\(\)\|\|([A-Za-z_$][A-Za-z0-9_$]*);if\(!\1\)/,
-          replacer: "$1=(0,$2)()||$3;debugger;if(false)"
+          replacer: "$1=(0,$2)()||$3;if(false)"
         },
         {
           pattern: /if\(\(0,([A-Za-z_$][A-Za-z0-9_$]*\.[A-Za-z_$][A-Za-z0-9_$]*)\)\(\)\)return;([A-Za-z_$][A-Za-z0-9_$]*)&&/,
-          replacer: "if((0,$1)())return;debugger;true&&"
+          replacer: "if((0,$1)())return;true&&"
         }
       ]
     },
@@ -30,6 +30,15 @@
         {
           pattern: /dispnf\.fyufotjpo;00|np{\.fyufotjpo;00/g,
           replacer: "FIGMA_PLEASE_STOP"
+        }
+      ]
+    },
+    {
+      markers: ["ext_init_wdf"],
+      replacements: [
+        {
+          pattern: "ext_init_wdf",
+          replacer: "__ext_init_wdf__"
         }
       ]
     }
