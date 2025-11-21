93dee01856e24b161e3fefffdcae6f0c1ab60f92
Justineo <justice360@gmail.com>
2025-11-21 14:39:40 +0800

    feat: update MCP section with new toggle functionality and improved layout; refactor icons and remove unused components

---
+++ components/icons/Collapsed.vue
@@ -0,0 +1,8 @@
+<template>
+  <svg class="tp-icon" width="16" height="16" fill="none" viewBox="0 0 16 16">
+    <path
+      fill="currentColor"
+      d="M6.768 5.525a.5.5 0 0 1 .707 0l2.121 2.121a.5.5 0 0 1 0 .707l-2.121 2.122a.5.5 0 0 1-.707-.708L8.535 8 6.768 6.232a.5.5 0 0 1 0-.707"
+    ></path>
+  </svg>
+</template>
